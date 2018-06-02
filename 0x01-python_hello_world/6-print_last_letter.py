169b1f8a4a22caf58cb7e2ebfaa1e76dd333cdd6
naishizhang <naishi.zhang@gmail.com>
2018-06-02 23:16:32 +0000

    add

--- 0x01-python_hello_world/6-print_last_letter.py
+++ 0x01-python_hello_world/6-print_last_letter.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 my_word = "Holberton"
-print("{0} is the last letter of the word {1}".format(my_word[8],my_word))
+print("{} is the last letter of the word {}".format(my_word[-1],my_word))
