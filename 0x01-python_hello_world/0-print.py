53056518c929756d18156391acf0e99a5751b176
naishizhang <naishi.zhang@gmail.com>
2018-06-02 21:23:16 +0000

    add

--- 0x01-python_hello_world/0-print.py
+++ 0x01-python_hello_world/0-print.py
@@ -1,3 +1,2 @@
-#!/user/bin/python3
-print("Programming is like building a multilingual puzzle")\n
-$ chmod +x myscript.py
+#!/usr/bin/python3
+print("Programming is like building a multilingual puzzle")
